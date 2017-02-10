c252134cd041d8cae600744b1b88bdcda7493d17
sooraj2102 <srjshingari@gmail.com>
2017-02-10 12:52:30 +0530

    splash

--- app/src/main/java/edu/kiet/www/epoque2017/Splash/SplashActivity.java
+++ app/src/main/java/edu/kiet/www/epoque2017/Splash/SplashActivity.java
@@ -2,6 +2,7 @@ package edu.kiet.www.epoque2017.Splash;
 
 
 
+        import android.content.Intent;
         import android.os.Bundle;
         import android.support.v4.app.Fragment;
         import android.support.v4.app.FragmentManager;
@@ -11,6 +12,7 @@ package edu.kiet.www.epoque2017.Splash;
         import android.widget.Button;
         import android.widget.LinearLayout;
 
+        import edu.kiet.www.epoque2017.Activity.Home;
         import edu.kiet.www.epoque2017.R;
         import edu.kiet.www.epoque2017.Fragment.LoginFragment;
         import edu.kiet.www.epoque2017.Fragment.RegisterFragment;
@@ -21,8 +23,8 @@ public class SplashActivity extends AppCompatActivity {
     FragmentTransaction ft;
     LinearLayout layout;
     FragmentManager fragmentManager;
-    Button login,register;
-
+    Button login;
+ int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,45 +34,24 @@ public class SplashActivity extends AppCompatActivity {
         ft = fragmentManager.beginTransaction();
         ft.add(new SplashFragment(), "fm");
 
-        ft.addToBackStack(null);
-        ft.commit();
+             ft.commit();
         layout=(LinearLayout)findViewById(R.id.layout_buttons);
         login=(Button)findViewById(R.id.login);
-        register=(Button)findViewById(R.id.register);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                fragmentManager.beginTransaction().add(R.id.fragment,new LoginFragment(), "LoginFragment").setTransition(
-                        FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
-                layout.setVisibility(View.GONE);
-                //ft.addToBackStack(null);
+                count++;
+                if(count==1)
+                fragmentManager.beginTransaction().add(R.id.fragment,new LoginFragment(), "LoginFragment").commit();
+                else
+                    startActivity(new Intent(SplashActivity.this,Home.class));
             }
         });
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fragmentManager.beginTransaction().add(R.id.fragment,new RegisterFragment(), "RegisterFragment").setTransition(
-                        FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
-                layout.setVisibility(View.GONE);
-            }
-        });
+
     }
     @Override
     public void onBackPressed(){
-        Fragment f= getSupportFragmentManager().getFragments().get(getSupportFragmentManager().getFragments().size()-1);
-        //Log.e("key",f.getClass().getName());
-        //Log.e("key2",LoginFragment.class.getName());
-
-        if(f.getClass().getName().equals(LoginFragment.class.getName())) {
-            fragmentManager.beginTransaction().add(R.id.fragment, new SplashFragment(), "SplashActivity").commit();
-            layout.setVisibility(View.VISIBLE);
-
-        }
-        else if(f.getClass().getName().equals(RegisterFragment.class.getName())) {
-            fragmentManager.beginTransaction().add(R.id.fragment, new SplashFragment(), "SplashActivity").commit();
-            layout.setVisibility(View.VISIBLE);
-        }
-        else
             super.onBackPressed();
 
 
