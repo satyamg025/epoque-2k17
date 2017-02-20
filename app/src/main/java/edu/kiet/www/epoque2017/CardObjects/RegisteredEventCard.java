f0c3893b4a575ea91bce95337b8ec06ae326ed1d
Shrey <shreymisra8@gmail.com>
2017-02-20 11:35:07 +0530

    commit

---
+++ app/src/main/java/edu/kiet/www/epoque2017/CardObjects/RegisteredEventCard.java
@@ -0,0 +1,10 @@
+package edu.kiet.www.epoque2017.CardObjects;
+
+/**
+ * Created by Shrey on 20-02-2017.
+ */
+
+public class RegisteredEventCard {
+    public String eventName;
+    public int eventPhoto;
+}
--- app/src/main/java/edu/kiet/www/epoque2017/Fragment/Profile.java
+++ app/src/main/java/edu/kiet/www/epoque2017/Fragment/Profile.java
@@ -5,27 +5,42 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.kiet.www.epoque2017.Adapters.EventAdapter;
+import edu.kiet.www.epoque2017.Adapters.RegisteredEventsAdapter;
+import edu.kiet.www.epoque2017.CardObjects.EventCardData;
+import edu.kiet.www.epoque2017.CardObjects.RegisteredEventCard;
 import edu.kiet.www.epoque2017.R;
 import edu.kiet.www.epoque2017.util.DbHandler;
 
 
 public class Profile extends Fragment {
-
+    private RecyclerView recyclerView;
+    private RegisteredEventsAdapter adapter;
+    Button logout;
     public Profile(){}
-    CardView registeredEvents,ourSponsors,logout,contactUs;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         final View view5= inflater.inflate(R.layout.fragment_profile, container, false);
-        logout=(CardView)view5.findViewById(R.id.logout);
-        registeredEvents=(CardView)view5.findViewById(R.id.regEvents);
-        ourSponsors=(CardView)view5.findViewById(R.id.sponsors);
-        contactUs=(CardView)view5.findViewById(R.id.contact);
+        recyclerView=(RecyclerView)view5.findViewById(R.id.registeredEventsRecyclerView);
+        adapter=new RegisteredEventsAdapter(getContext(),getData());
+        recyclerView.setAdapter(adapter);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(view5.getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        logout=(Button)view5.findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +62,18 @@ public class Profile extends Fragment {
         });
 
         return view5;
+    }
+    public static List<RegisteredEventCard> getData(){
+        List<RegisteredEventCard> data= new ArrayList<>();
+        int eventImage=R.drawable.splash_background;
+        String eventName="Event Name";
+        for(int i=0;i<5;i++) {
+            RegisteredEventCard current=new RegisteredEventCard();
+            current.eventName=eventName;
+            current.eventPhoto=eventImage;
+            data.add(current);
+        }
+        return data;
     }
 
 }
